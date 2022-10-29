08b0115fbb64dd2f71b0bfd84760d78cb6205a6c
davieid <aiyegbayo17@gmail.com>
2022-10-29 16:02:30 +0100

    express server setup for task 1

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+app = express(),
+PORT = 5100;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get( '/', ( req, res) => {
+    res.json({
+        "slackUsername": "maiyegbayo",
+        "backend": true,
+        "age": 24,
+        "bio": "A fullstack web developer based in Abuja, who believes his too cool for a bio 😎"
+    })
+})
+
+app.listen( PORT, () => {
+    console.log(`HNG server started on Port ${PORT}`)
+})
