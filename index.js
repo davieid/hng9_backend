b015398e617c4d89edc3f9b06dda16d31b61e12c
davieid <aiyegbayo17@gmail.com>
2022-11-05 11:19:39 +0100

    stage2

--- index.js
+++ index.js
@@ -13,6 +13,37 @@ app.get( '/', ( req, res) => {
     })
 })
 
+app.post( '/', ( req, res) => {
+    let op_type = req.body.operation_type,
+    x = req.body.x,
+    y = req.body.y;
+    switch (op_type) {
+        case "addition ":
+            res.json({
+                "slackUsername": "maiyegbayo",
+                "operation_type": op_type,
+                "result": x+y
+            })
+            break;
+        
+        case "subtraction ":
+            res.json({
+                "slackUsername": "maiyegbayo",
+                "operation_type": op_type,
+                "result": x-y
+            })
+            break;
+
+        default:
+            res.json({
+                "slackUsername": "maiyegbayo",
+                "operation_type": op_type,
+                "result": x*y
+            })
+            break;
+    }
+})
+
 app.listen( PORT, () => {
     console.log(`HNG server started on Port ${PORT}`)
 })
